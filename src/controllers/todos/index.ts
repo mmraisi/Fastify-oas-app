e7b82327ac1f49523002453e1e6ddeabf6924881
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-15 00:01:38 -0400

    feat(controllers): added the main handler

---
+++ src/controllers/todos/index.ts
@@ -0,0 +1,11 @@
+import { createTodo } from './createTodo';
+import { getTodoById } from './getTodoById';
+import { getTodos } from './getTodos';
+import { updateTodo } from './updateTodo';
+
+export const todoController = {
+  createTodo,
+  updateTodo,
+  getTodoById,
+  getTodos,
+};
