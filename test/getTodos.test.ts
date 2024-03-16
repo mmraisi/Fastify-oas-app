e825fddd6d30cce43af030c0c43e4c0d454922b1
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-15 22:24:54 -0400

    fix(unit-test): added describe method for todos

--- test/getTodos.test.ts
+++ test/getTodos.test.ts
@@ -1,13 +1,15 @@
-import { expect, test } from 'vitest';
+import { expect, test, describe } from 'vitest';
 import server from '../src/server';
 import { db } from '../src/database';
 
-test('GET all todos', async () => {
-  const response = await server.inject({
-    method: 'GET',
-    url: '/todos',
-  });
+describe('GET all todos', () => {
+  test('GET all todos', async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/todos',
+    });
 
-  expect(response.statusCode).toEqual(200);
-  expect(JSON.parse(response.body)).toEqual(db.todo);
+    expect(response.statusCode).toEqual(200);
+    expect(JSON.parse(response.body)).toEqual(db.todo);
+  });
 });
