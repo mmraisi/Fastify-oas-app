8d95873b58b245c66c43f98991b25ffbd0d401bb
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-22 14:51:42 -0400

    fix(semantic-release): fixed release config

---
+++ release.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+	branches: ['main'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/git',
+		[
+			'@semantic-release/github',
+			{
+				assets: [
+					{
+						path: 'dist/*.js',
+						label: 'JS Bundle',
+					},
+					{
+						path: 'dist/*.d.ts',
+						label: 'Type Definitions',
+					},
+				],
+			},
+		],
+	],
+};
