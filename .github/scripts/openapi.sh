133c33284e7c76cfb8f56e00ffcbcf0185f61cf8
mmraisi <mahmoudmraisi2@gmail.com>
2024-03-26 03:14:30 -0400

    chore(docker): updated redocly script

--- .github/scripts/openapi.sh
+++ .github/scripts/openapi.sh
@@ -3,5 +3,19 @@
 # Exit immediately if a command exits with a non-zero status
 set -eu
 
-# Bundle OpenAPI specification using redocly
+# Install required npm packages
+npm install -g --ignore-scripts --quiet @redocly/cli@latest nodemon
+
+# Function to check if redocly is available
+check_redocly() {
+    which redocly >/dev/null 2>&1
+}
+
+# Wait until redocly is available
+until check_redocly; do
+    echo "Waiting for redocly to be available..."
+    sleep 1
+done
+
+# Once redocly is available, bundle OpenAPI specification
 redocly bundle -d "/app/spec/openapi.yml" -o "/app/src/openapi.json"
